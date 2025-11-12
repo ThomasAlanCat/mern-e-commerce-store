7e3fb4c7b0534d5d863245cbfa7ac1120f995cb7
Thomas_Alan <nzampos@gmail.com>
2025-11-12 09:57:27 +0200

    update redis.js

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,3 +1,4 @@
+// upstash.com
 import Redis from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
@@ -5,7 +6,4 @@ dotenv.config();
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
 // key-value store
 
-
-
-
-//await redis.set("foo", "bar");
+// await redis.set("foo", "bar");
